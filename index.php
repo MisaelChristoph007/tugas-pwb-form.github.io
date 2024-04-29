63ef89d51f407be349c2e7c021735f27337244b3
0xMCH4CK007 <122249229+MisaelChristoph007@users.noreply.github.com>
2024-04-29 16:29:11 +0700

    Update and rename index.html to index.php
    Signed-off-by: 0xMCH4CK007 <122249229+MisaelChristoph007@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Media Online</h1>
+    <h3 style="font-family: 'Times New Roman', Times, serif;">Sosial Media Developer</h3>
+    <p>"Belajar dan berbagi agar hidup menjadi lebih baik" <br></p>
+    <h3>Benefit Join di Media Online</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama para Developer</li>
+        <li>Sharing Knowledge</li>
+        <li>Dibuat oleh calon Web Developer terbaik</li>
+    </ul>
+    <h3>Cara Bergabung ke Media Online</h3>
+    <ol>
+        <li>Mengunjungi website ini</li>
+        <li>Mendaftarkan di <a href="form.php" >Form Sign Up</a></li> 
+        <li>Selesai</li>
+    </ol>
+</div>
+</body>
+</html>
